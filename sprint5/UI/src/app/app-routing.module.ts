09fee8df3b830edff52320c21aecd771b453612d
mehras <mehrasmeydani@gmail.com>
2026-07-01 14:13:19 +0200

    laptop

--- sprint5/UI/src/app/app-routing.module.ts
+++ sprint5/UI/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   { path: 'auth', loadChildren: () => import(`./auth/auth.module`).then(m => m.AuthModule), data: { title: 'Authentication' } },
   { path: 'account', loadChildren: () => import(`./account/account.module`).then(m => m.AccountModule), data: { title: 'Account' } },
   { path: 'admin', loadChildren: () => import(`./admin/admin.module`).then(m => m.AdminModule), data: { title: 'Admin' } },
+  { path: 'imprint', loadComponent: () => import('./imprint/imprint.component').then(m => m.ImprintComponent), data: { title: 'Imprint' } },
 ];
 
 @NgModule({
---
+++ sprint5/UI/src/app/imprint/imprint.component.ts
@@ -0,0 +1,11 @@
+// Copyright (c) 2024-2026 Testsmith. All rights reserved.
+// See LICENSE for details.
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-imprint',
+  templateUrl: './imprint.component.html',
+  standalone: true,
+})
+export class ImprintComponent {}
